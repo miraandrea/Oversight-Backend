0c317bb6ccab495966fbeba72800dd626b56fe02
Kleverman Salazar Florez <klevermansalazar23@gmail.com>
2022-05-05 14:51:31 -0500

    Endpoint v3 has been created to create a new student
    * The enpoint v3 was created to create courses because in this version the
    
    * The listener of the index.js file has been modified, now it shows a message more

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ const app = require("./app");
 const PORT = process.env.PORT || 4000;
 
 // Listener
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`\n****************************************************************`);
+  console.log(`**************** ⚡Server running on port ${PORT}⚡ ***************`);
+  console.log(`****************************************************************\n`);
+});
