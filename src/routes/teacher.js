97f158ed0b36d56a50616aa96782fb64f32a5aeb
JGallego71 <johegari@gmail.com>
2022-05-16 13:32:13 -0500

    The endpoint was created to visualize the annotations made by a teacher

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -56,6 +56,22 @@ router.get("/v1/teachers/:name", (req, res) => {
   }
 });
 
+// Endpoint to view all the observations made by a teacher
+router.get("/v1/teachers/:document/observers", (req, res) => {
+  try {
+    const sql = `CALL pa_ver_anotaciones_del_docente (${req.params.document})`;
+    connection.query(sql, (error, results) => {
+      if (error) throw error;
+      if (results.length > 0) {
+        const token = jwt.sign({ results: results[0] }, tokenSignature);
+        return res.status(200).json(token);
+      }
+    });
+  } catch {
+    return res.status(500).json({ message: "System Error" });
+  }
+});
+
 // Api to add a new teacher in the system
 router.post("/v2/teachers", (req, res) => {
   try {
@@ -82,36 +98,46 @@ router.post("/v2/teachers", (req, res) => {
 });
 
 // Endpoint to add a new teacher in the system
-router.post("/v3/teachers",uploadImage.single("teacherImage"), async (req, res) => {
-  try {
-    const { documentTeacher, name, lastName, idAsignature, genre, signature } = await req.body;
-    const teacherPhoto = await cloudinary.uploader.upload(req.file.path);
-    const sql = `INSERT INTO docentes(documento,foto,nombre,apellido,idasignatura,genero,firma) VALUES(${documentTeacher},'${teacherPhoto.secure_url}','${name}','${lastName}',${idAsignature},'${genre}','${signature}')`;
-    connection.query(sql, (error) => {
-      if (error) throw error;
-      res.status(200).json({
-        registeredTeacher: true,
+router.post(
+  "/v3/teachers",
+  uploadImage.single("teacherImage"),
+  async (req, res) => {
+    try {
+      const {
+        documentTeacher,
+        name,
+        lastName,
+        idAsignature,
+        genre,
+        signature,
+      } = await req.body;
+      const teacherPhoto = await cloudinary.uploader.upload(req.file.path);
+      const sql = `INSERT INTO docentes(documento,foto,nombre,apellido,idasignatura,genero,firma) VALUES(${documentTeacher},'${teacherPhoto.secure_url}','${name}','${lastName}',${idAsignature},'${genre}','${signature}')`;
+      connection.query(sql, (error) => {
+        if (error) throw error;
+        res.status(200).json({
+          registeredTeacher: true,
+        });
       });
-    });
-  } catch{
-    res.status(401).json({ registeredTeacher: false });
+    } catch {
+      res.status(401).json({ registeredTeacher: false });
+    }
   }
-});
+);
 
 // Endpoint to add a new teacher in the system
-router.post("/v4/teachers",uploadImage.single("image"), async (req, res) => {
+router.post("/v4/teachers", uploadImage.single("image"), async (req, res) => {
   try {
     const { document, name, lastName, genre, signature } = await req.body;
     const photo = await cloudinary.uploader.upload(req.file.path);
     const sql = `INSERT INTO docentes(documento,foto,nombre,apellido,genero,firma) VALUES(${document},'${photo.secure_url}','${name}','${lastName}','${genre}','${signature}')`;
     connection.query(sql, (error) => {
       if (error) {
-        return res.status(200).json({registeredTeacher:false})
-      };
-      return res.status(200).json({registeredTeacher:true});
-    })
-  }
-  catch{
+        return res.status(200).json({ registeredTeacher: false });
+      }
+      return res.status(200).json({ registeredTeacher: true });
+    });
+  } catch {
     res.status(500).json({ message: "System Error" });
   }
 });
