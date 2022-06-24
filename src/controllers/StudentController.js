763b26af36cc9c2876abd8491e74a28b6f6130d9
Kleverman Salazar Florez <klevermansalazar23@gmail.com>
2022-06-24 15:48:53 -0500

    Creates the endpoint to delete a specific teacher

---
+++ src/controllers/StudentController.js
@@ -0,0 +1,35 @@
+class StudentController{
+    getStudent(){
+        try{
+            return "Get a specific student";
+        }
+        catch(e){
+            console.log(e);
+            return `Message: "Upsss, System Error"`;
+        }
+    }
+    createStudent(){
+        try{
+
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+    updateStudent(){
+        try{
+
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+    deleteStudent(){
+        try{
+
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+}
